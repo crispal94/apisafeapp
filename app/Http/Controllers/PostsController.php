1dddccf11a6ae77cea662ba193d7b47e569a39dd
Christian Palacios <crispal94@hotmail.com>
2019-05-02 15:44:50 -0500

    Cambios metodos api

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,181 +1,194 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
-use App\Post;
-use App\Imagen;
-use App\Referenciales;
-use App\Estadoanimo;
 use App\Comentarios;
+use App\Estadoanimo;
+use App\Imagen;
+use App\Post;
 use App\Reaccion;
-use Illuminate\Support\Facades\Storage;
+use App\Referenciales;
+use App\Seguidores;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
-use DB;
 
-class PostsController extends Controller {
+class PostsController extends Controller
+{
 
-public function addPost(Request $request){
-  $iduser = $request->input('iduser');
-  //$flag = $request->input('tipo');
-  $file = $request->file('file');
-  $idestado = $request->input('idestado');
-  $descripcion = $request->input('descripcion');
-  $estcomentario = $request->input('estcomentario');
+    public function addPost(Request $request)
+    {
+        $iduser = $request->input('iduser');
+        //$flag = $request->input('tipo');
+        $file = $request->file('file');
+        $idestado = $request->input('idestado');
+        $descripcion = $request->input('descripcion');
+        $estcomentario = $request->input('estcomentario');
 
-  //SI ES TIPO 1 ES TEXTO, TIPO 2 ES IMAGEN, TIPO 3 IMAGEN Y TEXTO
+        //SI ES TIPO 1 ES TEXTO, TIPO 2 ES IMAGEN, TIPO 3 IMAGEN Y TEXTO
 
-  if($file==null&&$descripcion!=null){
-  $post = new Post;
-  $post->id_user = $iduser;
-  $post->id_referenciales = 1;
-  $post->id_estadoanimo = $idestado;
-  $post->descripcion = $descripcion;
-  $post->est_comentario = $estcomentario;
-  $post->save();
-  $array = ['success'=>true,'message'=>'Post creado con exito'];
-  return response()->json($array);
-}else if($file!=null&&$descripcion==null){
+        if ($file == null && $descripcion != null) {
+            $post = new Post;
+            $post->id_user = $iduser;
+            $post->id_referenciales = 1;
+            $post->id_estadoanimo = $idestado;
+            $post->descripcion = $descripcion;
+            $post->est_comentario = $estcomentario;
+            $post->save();
+            $array = ['success' => true, 'message' => 'Post creado con exito'];
+            return response()->json($array);
+        } else if ($file != null && $descripcion == null) {
 
-  $idfinal = Imagen::where('id_user',$iduser)->max('id');
-  $imagen = new Imagen;
-  $imagen->id_referenciales = 4;
-  $nombreimagen = 'post'.$iduser.($idfinal+1).'.jpg';
-  $imagen->descripcion = $nombreimagen;
-  $imagen->id_user = $iduser;
-  $imagen->save();
+            $idfinal = Imagen::where('id_user', $iduser)->max('id');
+            $imagen = new Imagen;
+            $imagen->id_referenciales = 4;
+            $nombreimagen = 'post' . $iduser . ($idfinal + 1) . '.jpg';
+            $imagen->descripcion = $nombreimagen;
+            $imagen->id_user = $iduser;
+            $imagen->save();
 
-  $destinationPath = '/var/www/html/apisafeapp/public/img-post';
-  $file->move($destinationPath,$nombreimagen);
+            $destinationPath = '/var/www/html/apisafeapp/public/img-post';
+            $file->move($destinationPath, $nombreimagen);
 
-  $post = new Post;
-  $post->id_user = $iduser;
-  $post->id_referenciales = 2;
-  $post->id_imagen = $imagen->id;
-  $post->descripcion = 'post con imagen';
-  $post->id_estadoanimo = $idestado;
-  $post->est_comentario = $estcomentario;
-  $post->save();
-  $array = ['success'=>true,'message'=>'Post creado con exito'];
-  return response()->json($array);
+            $post = new Post;
+            $post->id_user = $iduser;
+            $post->id_referenciales = 2;
+            $post->id_imagen = $imagen->id;
+            $post->descripcion = 'post con imagen';
+            $post->id_estadoanimo = $idestado;
+            $post->est_comentario = $estcomentario;
+            $post->save();
+            $array = ['success' => true, 'message' => 'Post creado con exito'];
+            return response()->json($array);
 
-}else if($file!=null&&$descripcion!=null){
-    $idfinal = Imagen::where('id_user',$iduser)->max('secuencial_img');
-    $nextsecuencial = $idfinal + 1;
-    $imagen = new Imagen;
-    $imagen->id_referenciales = 4;
-    $nombreimagen = 'post'.$iduser.$nextsecuencial.'.jpg';
-    $imagen->descripcion = $nombreimagen;
-    $imagen->secuencial_img = $nextsecuencial;
-    $imagen->id_user = $iduser;
-    $imagen->save();
+        } else if ($file != null && $descripcion != null) {
+            $idfinal = Imagen::where('id_user', $iduser)->max('secuencial_img');
+            $nextsecuencial = $idfinal + 1;
+            $imagen = new Imagen;
+            $imagen->id_referenciales = 4;
+            $nombreimagen = 'post' . $iduser . $nextsecuencial . '.jpg';
+            $imagen->descripcion = $nombreimagen;
+            $imagen->secuencial_img = $nextsecuencial;
+            $imagen->id_user = $iduser;
+            $imagen->save();
 
-    $destinationPath = '/var/www/html/apisafeapp/public/img-post';
-    $file->move($destinationPath,$nombreimagen);
+            $destinationPath = '/var/www/html/apisafeapp/public/img-post';
+            $file->move($destinationPath, $nombreimagen);
 
-    $post = new Post;
-    $post->id_user = $iduser;
-    $post->id_referenciales = 3;
-    $post->id_imagen = $imagen->id;
-    $post->descripcion = $descripcion;
-    $post->id_estadoanimo = $idestado;
-    $post->est_comentario = $estcomentario;
-    $post->save();
-    $array = ['success'=>true,'message'=>'Post creado con exito'];
-    return response()->json($array);
+            $post = new Post;
+            $post->id_user = $iduser;
+            $post->id_referenciales = 3;
+            $post->id_imagen = $imagen->id;
+            $post->descripcion = $descripcion;
+            $post->id_estadoanimo = $idestado;
+            $post->est_comentario = $estcomentario;
+            $post->save();
+            $array = ['success' => true, 'message' => 'Post creado con exito'];
+            return response()->json($array);
 
-  }
+        }
 
-}
-
-
-public function getPost(Request $request,$id){
-    $posts = Post::where('id_user',$id)->orderBy('created_at','desc')->get();
-    $jsontotal = [];
-    foreach ($posts as $post) {
-      $tmp = [];
-      $tmp['fecha'] = $post->created_at->toDateString();
-      $referencial = Referenciales::find($post->id_referenciales);
-      $tmp['tipo_post'] = $referencial->descripcion;
-      $tmp['contenido'] = $post->descripcion;
-      if($post->imagen!=null){
-      $imagen = $post->imagen->descripcion;
-      }else{
-      $imagen = 'post sin imagen';
-      }
-      $tmp['imagen'] = $imagen;
-      $estadoanimo = Estadoanimo::find($post->id_estadoanimo);
-      $tmp['estadoanimo'] = $estadoanimo->descripcion;
-      array_push($jsontotal,$tmp);
-      $cont = count($posts);
     }
-    return response()->json(['success'=>true,'totalposts'=>$cont,'data'=>$jsontotal]);
-}
 
+    public function getPost(Request $request, $id)
+    {
+        $posts = Post::where('id_user', $id)->orderBy('created_at', 'desc')->get();
+        $jsontotal = [];
+        foreach ($posts as $post) {
+            $tmp = [];
+            $tmp['fecha'] = $post->created_at->toDateString();
+            $referencial = Referenciales::find($post->id_referenciales);
+            $tmp['tipo_post'] = $referencial->descripcion;
+            $tmp['contenido'] = $post->descripcion;
+            if ($post->imagen != null) {
+                $imagen = $post->imagen->descripcion;
+            } else {
+                $imagen = 'post sin imagen';
+            }
+            $tmp['imagen'] = $imagen;
+            $estadoanimo = Estadoanimo::find($post->id_estadoanimo);
+            $tmp['estadoanimo'] = $estadoanimo->descripcion;
+            array_push($jsontotal, $tmp);
+            $cont = count($posts);
+        }
+        return response()->json(['success' => true, 'totalposts' => $cont, 'data' => $jsontotal]);
+    }
 
-public function getPostAll(){
-  $posts = Post::all();
-  $jsontotal = [];
-  foreach ($posts as $post) {
-    $tmp = [];
-    $tmp['fecha'] = $post->created_at->toDateString();
-    $referencial = Referenciales::find($post->id_referenciales);
-    $tmp['tipo_post'] = $referencial->descripcion;
-    $tmp['contenido'] = $post->descripcion;
-    $imagen = $post->imagen->descripcion;
-    $tmp['imagen'] = $imagen;
-    $estadoanimo = Estadoanimo::find($post->id_estadoanimo);
-    $tmp['estadoanimo'] = $estadoanimo->descripcion;
-    array_push($jsontotal,$tmp);
-  }
-  return response()->json($jsontotal);
-}
+    public function getPostAll(Request $request)
+    {
+        $id_user = $request->input('id_user');
+        $followers = Seguidores::where('id_user_padre', $id_user)->get();
+        $array_users = [];
+        foreach ($followers as $fo) {
+            array_push($array_users, $fo->id_user_seguidor);
+        }
+        array_push($array_users, $id_user);
+        $posts = Post::whereIn('id_user', $array_users)->get();
+        //dd($posts);
+        $jsontotal = [];
+        foreach ($posts as $post) {
+            $tmp = [];
+            $tmp['fecha'] = $post->created_at->toDateString();
+            $referencial = Referenciales::find($post->id_referenciales);
+            $tmp['tipo_post'] = $referencial->descripcion;
+            $tmp['contenido'] = $post->descripcion;
+            if ($post->imagen != null) {
+                $imagen = $post->imagen->descripcion;
+            } else {
+                $imagen = null;
+            }
+            $tmp['imagen'] = $imagen;
+            $estadoanimo = Estadoanimo::find($post->id_estadoanimo);
+            $tmp['estadoanimo'] = $estadoanimo->descripcion;
+            array_push($jsontotal, $tmp);
+        }
+        return response()->json($jsontotal);
+    }
 
+    public function postComentario(Request $request)
+    {
+        $iduser = $request->input('iduser');
+        $idpost = $request->input('idpost');
+        $descripcion = $request->input('descripcion');
 
-public function postComentario(Request $request){
-    $iduser = $request->input('iduser');
-    $idpost = $request->input('idpost');
-    $descripcion = $request->input('descripcion');
+        $comentario = new Comentarios();
+        $comentario->id_user = $iduser;
+        $comentario->id_post = $idpost;
+        $comentario->descripcion = $descripcion;
+        $comentario->save();
 
-    $comentario = new Comentarios();
-    $comentario->id_user = $iduser;
-    $comentario->id_post = $idpost;
-    $comentario->descripcion = $descripcion;
-    $comentario->save();
+        return response()->json(['success' => true, 'message' => 'Comentario creado con exito']);
+    }
 
-      return response()->json(['success'=>true,'message'=>'Comentario creado con exito']);
-}
+    public function postReaccion(Request $request)
+    {
+        $iduser = $request->input('iduser');
+        $idpost = $request->input('idpost');
+        $descripcion = $request->input('descripcion');
 
-public function postReaccion(Request $request){
-    $iduser = $request->input('iduser');
-    $idpost = $request->input('idpost');
-    $descripcion = $request->input('descripcion');
+        $reaccion = new Reaccion();
+        $reaccion->id_user = $iduser;
+        $reaccion->id_post = $idpost;
+        $reaccion->save();
 
-    $reaccion = new Reaccion();
-    $reaccion->id_user = $iduser;
-    $reaccion->id_post = $idpost;
-    $reaccion->save();
+        return response()->json(['success' => true, 'message' => 'Reaccion creada con exito']);
+    }
 
-      return response()->json(['success'=>true,'message'=>'Reaccion creada con exito']);
-}
+    public function getComentariosPost(Request $request)
+    {
+        $iduser = $request->input('iduser');
+        $idpost = $request->input('idpost');
 
+        $comentarios = Comentarios::where('id_user', $iduser)->where('id_post', $idpost)->get();
 
-public function getComentariosPost(Request $request){
-  $iduser = $request->input('iduser');
-  $idpost = $request->input('idpost');
+        return response()->json($comentarios);
+    }
 
-  $comentarios = Comentarios::where('id_user',$iduser)->where('id_post',$idpost)->get();
+    public function getReaccionesPost(Request $request)
+    {
+        $iduser = $request->input('iduser');
+        $idpost = $request->input('idpost');
 
-  return response()->json($comentarios);
-}
+        $reacciones = Reaccion::where('id_user', $iduser)->where('id_post', $idpost)->get();
 
-
-public function getReaccionesPost(Request $request){
-  $iduser = $request->input('iduser');
-  $idpost = $request->input('idpost');
-
-  $reacciones = Reaccion::where('id_user',$iduser)->where('id_post',$idpost)->get();
-
-  return response()->json($reacciones);
-}
+        return response()->json($reacciones);
+    }
 
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    (new Dotenv\Dotenv(__DIR__.'/../'))->load();
+    (new Dotenv\Dotenv(__DIR__ . '/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
@@ -17,17 +17,15 @@ try {
 | that serves as the central piece of this framework. We'll use this
 | application as an "IoC" container and router for this framework.
 |
-*/
+ */
 
 $app = new Laravel\Lumen\Application(
-    realpath(__DIR__.'/../')
+    realpath(__DIR__ . '/../')
 );
 
- $app->withFacades();
+$app->withFacades();
 
- $app->withEloquent();
-
-
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -38,7 +36,7 @@ $app = new Laravel\Lumen\Application(
 | register the exception handler and the console kernel. You may add
 | your own bindings here if you like or you can make another file.
 |
-*/
+ */
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
@@ -59,15 +57,15 @@ $app->singleton(
 | be global middleware that run before and after each request into a
 | route or middleware that'll be assigned to some specific routes.
 |
-*/
+ */
 
 // $app->middleware([
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
 $app->routeMiddleware([
-     'auth' => App\Http\Middleware\Authenticate::class,
- ]);
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -78,17 +76,19 @@ $app->routeMiddleware([
 | are used to bind services into the container. Service providers are
 | totally optional, so you are not required to uncomment this line.
 |
-*/
+ */
 
-  $app->register(App\Providers\AppServiceProvider::class);
-   $app->register(App\Providers\AuthServiceProvider::class);
-   $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
-   $app->register(Barryvdh\Cors\ServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Barryvdh\Cors\ServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-if (env('APP_DEBUG')) {
- $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
-}
+//if (env('APP_DEBUG')) {
 $app->configure('debugbar');
+
+$app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
+//}
+
 $app->configure('cors');
 
 $app->middleware([
@@ -106,12 +106,12 @@ $app->middleware([
 | the application. This will provide all of the URLs the application
 | can respond to, as well as the controllers that may handle them.
 |
-*/
+ */
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
 
 return $app;
